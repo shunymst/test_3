65027f55ea6430d1beeda11738f0bcdb7715a87e
shunymst <shunjiymst@gmail.com>
2018-01-20 22:25:14 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) # 今どれくらい処理が進んでるか確認する用
+
+#def similarWords(keyword):
+#	model = word2vec.Word2Vec.load(input)# load result
+#	return model.most_similar(positive=[keyword])
+
+if __name__=="__main__":
+	#out=similarWords(keyword)
+	print ("test start.")
+
